d5f657ce11c78b301166fa964169fa41fe0398e7
Eduardo Brito <edu.santos.brito@gmail.com>
2019-03-17 16:07:47 -0300

    Componente do botão finalizado - Apenas cor primária

---
+++ src/app/shared/button/button-component.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponentComponent } from './button-component.component';
+
+@NgModule({
+  declarations: [ButtonComponentComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ButtonComponentComponent
+  ]
+})
+export class ButtonComponentModule { }
