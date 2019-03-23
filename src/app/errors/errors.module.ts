9665ddc75e1753e41ef4f2f2337e186f52a16d67
Eduardo Brito <edu.santos.brito@gmail.com>
2019-03-23 20:45:27 -0300

    Página 404 + Algumas alterações nos componentes

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SharedButtonModule } from '../shared/components/button/shared-button.module';
 
 @NgModule({
   declarations: [NotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedButtonModule
   ]
 })
 export class ErrorsModule { }
