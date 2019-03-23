bd51a68d9c4193d10200ee7b8b985a6a076dcb85
Arthur Wagenknecht <oarthurdev@gmail.com>
2019-03-23 17:33:46 -0300

    Redirecionando da rota padrão '/' para a rota de login '/login'.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, //REDIRECIONANDO DA ROTA PADRÃO "/" PARA "/LOGIN".
   { path: 'login', component: LoginComponent }
 ];
 
