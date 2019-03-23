2084eda2095e6811ca9856e08009b56930863735
Xunjin <xunjin.coder@gmail.com>
2019-03-23 12:44:28 -0300

    correção app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './components/login/login.module';
 import { ErrorsModule } from './errors/errors.module';
-import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
   ],
   imports: [
     BrowserModule,
